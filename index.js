f8b3980b6babd97c8f9e3db48f77096eafe80275
陈思全 <98014938+siquan001@users.noreply.github.com>
2023-07-19 17:06:29 +0800

    Add files via upload

--- index.js
+++
@@ -1,8 +0,0 @@
-var ab=document.querySelector('[name=\'devh\']');
-ab.onclick=function(){
-  if(this.checked){
-    document.querySelector(".fold[data-name=\""+this.getAttribute('data-for')+"\"]").style.display='block';
-  }else{
-    document.querySelector(".fold[data-name=\""+this.getAttribute('data-for')+"\"]").style.display='none';
-  }
-}
