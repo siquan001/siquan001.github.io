e4dacf27106babd49bae244d3713afd1eb92c58f
siquan001 <chensiquan2022@163.com>
2025-03-30 15:37:41 +0800

    update comment

--- index.js
+++ index.js
@@ -61,6 +61,20 @@ function initRouter() {
         if (hash == 'intro') {
             initSkills();
         }
+
+        if(hash=='contact'){
+            (function(d, s) {
+                var j, e = d.getElementsByTagName(s)[0];
+         
+                if (typeof LivereTower === 'function') { return; }
+         
+                j = d.createElement(s);
+                j.src = 'https://cdn-city.livere.com/js/embed.dist.js';
+                j.async = true;
+         
+                e.parentNode.insertBefore(j, e);
+            })(document, 'script');
+        }
     }
     clhash();
 }
